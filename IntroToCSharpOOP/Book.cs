af8cc6d416e5574a3910bbf33c016d71a1d4b642
EthanAmato <ethannamato@gmail.com>
2023-02-23 14:48:03 -0500

    book class demonstrating oop in csharp

---
+++ IntroToCSharpOOP/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToCSharpOOP
+{
+    class Book
+    {
+        public string title;
+        public string description;
+        public string author;
+        public int pages;
+
+        public void read()
+        {
+            Console.WriteLine(this.pages);
+        }
+
+
+        public Book(string title, string description, string author, int pages) {
+            this.title = title;
+            this.description = description;
+            this.author = author;
+            this.pages = pages;
+        }
+
+    
+    }
+}
---
+++ IntroToCSharpOOP/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToCSharpOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Harry Potter", "Book about some kid", "J.K. Rowling", 500);
+            book.read();
+        }
+    }
+}
